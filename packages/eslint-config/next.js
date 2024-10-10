455c2f215f89fd1cfa2666d565fd4ed6f39f8e34
Turbobot <turbobot@vercel.com>
2024-10-09 23:14:18 -0400

    feat(create-turbo): apply official-starter transform

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -4,15 +4,21 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "turbo"],
-  plugins: ["only-warn"],
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    require.resolve("@vercel/style-guide/eslint/next"),
+    "turbo",
+  ],
   globals: {
     React: true,
     JSX: true,
   },
   env: {
     node: true,
+    browser: true,
   },
+  plugins: ["only-warn"],
   settings: {
     "import/resolver": {
       typescript: {
@@ -24,11 +30,6 @@ module.exports = {
     // Ignore dotfiles
     ".*.js",
     "node_modules/",
-    "dist/",
   ],
-  overrides: [
-    {
-      files: ["*.js?(x)", "*.ts?(x)"],
-    },
-  ],
+  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
 };
